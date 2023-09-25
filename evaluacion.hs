eed11b79af2ea0f10d67dd81a0c29b877a7385ad
brunardo19 <balderramab19@gmail.com>
2023-09-24 21:42:59 -0300

    Update evaluacion.hs

--- evaluacion.hs
+++ evaluacion.hs
@@ -8,6 +8,7 @@ mainPersona = do
     list <- ingreso []
     putStrLn ("Personas ingresadas") 
     mostrar list
+    promedio list 0 0 (length list)
 
 ingreso :: [(String,Int,Float)] -> IO [(String,Int,Float)]
 ingreso xs = do
@@ -33,6 +34,20 @@ mostrar xs = do
 mostrarPersona :: (String, Int, Float) -> IO ()
 mostrarPersona (nombre, edad, altura) = do
     putStrLn $ "Nombre: " ++ show nombre ++ "           " ++  "Edad: " ++ show edad ++ "           " ++ "Altura: " ++ show altura
+
+promedio :: [(String, Int, Float)] -> Int -> Float -> Int -> IO ()
+promedio [] ed al len = do
+    
+    let a = ed/len
+    let b = al/ fromIntegral len
+    putStrLn $ ("Promedio de edad : " ++ show (ed/len) ++ "Promedio de altura: " ++ show b)
+promedio ((nombre, edad, altura):xs) ed al len= do
+    let ed = ed + edad
+    let al = al + altura
+    promedio xs ed al len
+
+
+
 
 aDigitos :: Integer -> [Integer]
 aDigitos 0 = []
